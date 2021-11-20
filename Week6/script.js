1972a648b8bcdafe50ec950c13cf5ebf8020e9a3
WoodKT <soulsearcher14@gmail.com>
2021-11-20 03:09:15 -0500

    Update script.js
    total rebuild- Current commit has valid output. Need to add tie handling, score, and possible player class again

--- Week6/script.js
+++ Week6/script.js
@@ -8,202 +8,135 @@ class Card {
 
 class Deck {
     constructor() {
-        this.deck = [];
-        this.reset();
-        this.shuffle();
-//        this.deal();
-//        this.hand = [];
+        this.cards = [];
+        this.hand1 = [];
+        this.hand2 = [];
     }
 
-    reset(){
-        this.deck = [];
+    createDeck(){
+      const suits = ['clubs', 'diamonds', 'hearts', 'spades'];
+      const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+      const values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 
-        const suits = ['clubs', 'diamonds', 'hearts', 'spades'];
-        const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-
-        for (let suit in suits) {
-            for (let rank in ranks) {
-              this.deck.push(`${ranks[rank]} of ${suits[suit]}`);
-            }
-          }
+      for(var a = 0; a < suits.length; a++){
+        for(var b = 0; b < ranks.length; b++){
+          this.cards.push( new Card(suits[a], ranks[b], values[b]));
+        }
+      }
     }
-
-          shuffle() {
-            const { deck } = this;
-            let m = deck.length, i;
-        
-            while(m){
-              i = Math.floor(Math.random() * m--);
-        
-              [deck[m], deck[i]] = [deck[i], deck[m]];
-            }
-        
-            return this;
-          }
-        
-          deal(){
-//            let cardsPerPerson = 26;
-             let hand1 = this.deck.slice(0,26);
-             let hand2 = this.deck.slice(26, 52);
-             let hand = [];
-             hand.push(hand1);
-             hand.push(hand2);
-            return hand;
-          }
+    //https://www.geeksforgeeks.org/shuffle-a-given-array-using-fisher-yates-shuffle-algorithm/
+    shuffle(array){
+      var c = array.length, e,d;
+      while(c){
+        d = Math.floor(Math.random()* c--);
+        e = array[c];
+        array[c] = array[d];
+        array[d] = e;
+      }
+      return array;
     }
-  
-
-const deck1 = new Deck();
-console.log(deck1.deck);
-deck1.reset()
-console.log(deck1.deck);
-
-class Player {
-    constructor(name) {
-        this.name = name;
-        this.score = 0;
-        this.hand = [];
-    }
-    
-    addCards(decks){
-      this.hand.push(decks);
-    }
-
-    playCard(){
-      return this.hand.pop();
-    }
-
-    placeCard (card) {
-      let totalSpaces = this.hand.length + 1
-      let randomIndex = Math.floor(Math.random() * totalSpaces)
-      this.hand.splice(randomIndex, 0, card)
-    }
-
-    createPlayer() {
-      let name = prompt('Enter name for new player:');
-      this.players.push(new Player(name));
+       dealCards(){
+        this.hand1 = this.cards.slice(0,26);
+        this.hand2 = this.cards.slice(26, 52);
+      }
   }
-
-  /*
-    //MOCHA/CHAI testing
-    getPlayerName() {
-        return this.name;
-    }
-    */
-    //method to increment score
-    incrementScore(){
-        this.score += 1;
-    }
-
-}
-
-
 
 class Board {
     constructor() {
-        this.deck = new Deck();
-        this.player1 = new Player(1);
-        this.player2 = new Player(2);
-
-        this.startBoard();
+      this.newDeck = new Deck();
+      this.playedCards = [];
+      this.round = 1;
     }
 
     startBoard() {
-      let hands = this.deck.deal();
-      let hand1 = hands[0];
-      let hand2 = hands[1];
-
-      hand1.forEach((card) => {
-        this.player1.addCards(card)
-      })
-
-      hand2.forEach((card) => {
-        this.player2.addCards(card)
-      })
+      this.newDeck.createDeck();
+      this.newDeck.shuffle(this.newDeck.cards);
+      this.newDeck.dealCards();
     }
 
-    boardRound(){
-      let player1Card = this.player1.playCard();
-      let player2Card = this.player2.playCard();
-
-      console.log(`
-      Player 1 plays ${player1Card.rank} of ${player1Card.suit}.
-      Player 2 plays ${player2Card.rank} of ${player1Card.suit}.
-    `)
-
-    if (player1Card.value === player2Card.value) {
-      if (this.player1.hand.length === 0) {
-        console.log('Tie :(')
-      } else {
-        console.log('Tie :(')
-        this.player1.placeCard(player1Card)
-        this.player2.placeCard(player2Card)
+    playCard(){
+      if( this.newDeck.hand1.length > 0 && this.newDeck.hand2.length > 0){
+        this.playedCards.push(this.newDeck.hand1.pop());
+        this.playedCards.push(this.newDeck.hand2.pop());
       }
-    } else if (player1Card.value === 1) {
-      console.log('Player 1 wins point!')
-      this.player1.incrementScore(1)
-    } else if (player2Card.value === 1) {
-      console.log('Player 2 wins point!')
-      this.player2.incrementScore(1)
-    } else if (player1Card.value > player2Card.value) {
-      console.log('Player 1 wins point!')
-      this.player1.incrementScore(1)
-    } else if (player2Card.value > player1Card.value) {
-      console.log('Player 2 wins point!')
-      this.player2.incrementScore(1)
+      else if (this.newDeck.hand1.length === 0){
+        for(let f = 0; f < this.playedCards.length; f++){
+        this.newDeck.hand2.unshift(this.playedCards[f]);
+        }
+        console.log(`Player 2 wins`);
+      }
+      else if(this.newDeck.hand2.length === 0){
+        for(let g = 0; g < this.playedCards.length; g++){
+          this.newDeck.hand1.unshift(this.playedCards[g]);
+        }
+        console.log(`Player 1 wins`);
+      }
     }
 
+    compareCards(){
+      if( this.playedCards.length > 0){ 
+      if(this.playedCards[0].value > this.playedCards[1].value){ 
+        console.log(`Player 1 wins this round!`);
+        for(let h = 0; h < this.playedCards.length; h++){
+          this.newDeck.hand1.unshift(this.playedCards[h]);
+        }
+        this.clear();
+      }
+      else if(this.playedCards[0].value < this.playedCards[1].value){ 
+        console.log(`Player 2 wins this round`);
+        for(let i = 0; i < this.playedCards.length; i++){
+          this.newDeck.hand2.unshift(this.playedCards[i]);
+        }
+        this.clear();
+      }
+      console.log(`Player 1 has ${this.newDeck.hand1.length} card(s). Player 2 has ${this.newDeck.hand2.length} card(s).`); 
+      this.clear();
+      }
+      else{
+        if(this.newDeck.hand1.length > this.newDeck.hand2.length){
+          console.log(`Player 1 Wins!`);
+        }
+        else{
+          console.log(`Player 2 Wins!`);
+        }
+      }
     }
-}
 
-let board = new Board();
-board.startBoard();
+    clear(){
+      this.playedCards = [];
+    }
 
+  playRound(){
+    this.playCard();
+    this.compareCards();
+  }
 
+  playGame(){
+    while(this.newDeck.hand1.length > 20 && this.newDeck.hand2.length > 20 ){
+      this.playRound();
+      this.round++;
+    }
 
+    if(this.newDeck.hand1.length > this.newDeck.hand2.length){
+      console.log(`Player 1 wins!`);
+    }
+    else if (this.newDeck.hand1.length < this.newDeck.hand2.length){
+      console.log(`Player 2 wins!`);
+    }
+  }
+    
+    }
 
-
-
-
-    // createDeck() {
-    //     const suits = ['clubs', 'diamonds', 'hearts', 'spades'];
-    //     const ranks = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king'];
-    //     let values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-
-    //     for (let i = 0; i < suits.length; i++) {
-    //         for (let j = 0; j < ranks.length; j++) {
-    //             this.cards.push(new Card(suits[i], ranks[j], values[j]));
-    //         }
-    //     }
-    // }
-
-
-    // shuffleDeck() {
-    //     let location1, location2, tmp;
-    //     for (let i = 0; i < 1000; i++) {
-    //         location1 = Math.floor((Math.random() * this.cards.length));
-    //         tmp = this.cards[location1]; this.cards[location1] = this.cards[location2]; this.cards[location2] = tmp;
-    //     }
-    // }
+    let myBoard = new Board();
+    myBoard.startBoard();
+    console.log(myBoard.playGame());
 
 /*
-    class Board {
-      constructor() {
-          this.cardsInMiddle = [];
-          this.players = [];
-      }
-      start() {
-          this.players.push('');
-          this.players.push('');
-          let d = new Deck();
-          d.createDeck();
-          d.shuffleDeck();
-          this.players[0].playerCards = d.cards.slice(0, 26);
-          this.players[1].playerCards = d.cards.slice(26, 52);
-      }
+      class Player {
+    constructor(name) {
+        this.name = name;
+        this.value = 0;
+        this.hand = [];
+    }
   }
-  
-  let gameBoard = new Board();
-  gameBoard.start();
-
   */
