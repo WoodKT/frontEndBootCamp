f8cb1d9b112d5a9f71c592abf36e2435dc92856b
WoodKT <soulsearcher14@gmail.com>
2021-11-21 21:44:04 -0500

    add unit test js and html
    practice unit test

--- Week6/index_test.js
+++ Week6/index_test.js
@@ -1,6 +1,16 @@
 var expect = chai.expect;
 
-describe('', function()
-    describe('', function(){
-        
-    }))
+describe('MyFunctions', function(){
+    describe('#doSomething', function(){
+        it('concatenate parameters', function(){
+            var x = doSomething('hi', 2);
+            expect(x).to.equal('hi2');
+        });
+
+        it('throw error if first param not string', function(){
+            expect(function(){
+                doSomething(2, 2);
+            }).to.throw(Error);
+        });
+    });
+});
