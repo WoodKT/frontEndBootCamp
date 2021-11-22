27b12e095b3c22135f0ec6ce8c9a34ff61ef84d9
WoodKT <soulsearcher14@gmail.com>
2021-11-21 23:53:37 -0500

    first unit test
    fixed .js link to script on html file, wrote first unit test

--- Week6/index_test.js
+++ Week6/index_test.js
@@ -1,16 +1,17 @@
 var expect = chai.expect;
 
-describe('MyFunctions', function(){
-    describe('#doSomething', function(){
-        it('concatenate parameters', function(){
-            var x = doSomething('hi', 2);
-            expect(x).to.equal('hi2');
+describe('week6', function(){
+    describe('#createDeck', function(){
+        it('create deck of 52', function(){
+            const d = new Deck();
+            d.createDeck();
+            expect(d.cards).length(52); 
         });
 
-        it('throw error if first param not string', function(){
-            expect(function(){
-                doSomething(2, 2);
-            }).to.throw(Error);
-        });
+        // it('throw error if not 52', function(){
+        //     expect(function(){
+        //         doSomething(2, 2);
+        //     }).to.throw(Error);
+        // });
     });
 });
