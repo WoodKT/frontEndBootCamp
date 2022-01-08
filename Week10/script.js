bf8019ba32ccd5f9ba2d3d5a4c74531ae0663492
WoodKT <soulsearcher14@gmail.com>
2022-01-08 04:09:53 -0500

    finish up
    css, content

--- Week10/script.js
+++ Week10/script.js
@@ -8,8 +8,7 @@ document.getElementById('add').addEventListener('click', () => {
     row.insertCell(0).innerHTML = document.getElementById('new-event').value;
     row.insertCell(1),innerHTML = `${createdDate.getFullYear()}-${createdDate.getMonth() + 1}-${createdDate.getDate()}`;
     row.insertCell(2).innerHTML = document.getElementById('new-start-date').value;
-    row.insertCell(3).innerHTML = document.getElementById('new-end-date').value;
-    let actions = row.insertCell(4);
+    let actions = row.insertCell(3);
     actions.appendChild(createDeleteButton(id++));
     document.getElementById('new-event').value = '';
 });
