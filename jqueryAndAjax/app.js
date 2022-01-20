390c49f71807be08be3d82508e3fb085dccdd85f
WoodKT <soulsearcher14@gmail.com>
2022-01-20 14:46:51 -0500

    tic tac toe
    working state

--- jqueryAndAjax/app.js
+++ jqueryAndAjax/app.js
@@ -1,16 +1,14 @@
-let gameStop = false;
+let gameEnd = false;
 let turnState = 1;
-let gameStat = ['NO WINNER','','','','','','','','',''];
+let moves = ['NONE','','','','','','','','',''];
 
 onClick('clear', () => {  
     let headerText = document.getElementById('turn');
-    gameStat = ['NO WINNER','','','','','','','','',''];
+    moves = ['NONE','','','','','','','','',''];
     turnState = 1;
-    gameStop = false;
+    gameEnd = false;
     drawDOM();
-    console.log('Game Reset!');
     headerText.innerHTML = "New Game, X's turn";
-
 });
 
 function onClick(id, action) {
@@ -20,42 +18,37 @@ function onClick(id, action) {
 }
 
 function drawDOM() {
-    let tictactoeDiv = document.getElementById('board');
-    for (let index=1; index<=9; index++ ) {
-        document.getElementById(`b-${index}`).innerHTML=' ';
+    let ticTac = document.getElementById('board');
+    for (let i=1; i<=9; i++ ) {
+        document.getElementById(`b-${i}`).innerHTML=' ';
     }
 }
 
-for (let btnIndex=1; btnIndex <=9; btnIndex++) {
-    onClick(`b-${btnIndex}`, () => {
-        if (!gameStop) {
+for (let moveI=1; moveI <=9; moveI++) {
+    onClick(`b-${moveI}`, () => {
+        if (!gameEnd) {
             let headerText = document.getElementById('turn');
-            console.log(gameStat);
-            if (gameStat[btnIndex] == '') {
+            if (moves[moveI] == '') {
                 console.log(`Turn #${turnState}:`);  
                 if (turnState % 2) {
-                    document.getElementById(`b-${btnIndex}`).innerHTML = 'X'
-                    gameStat[btnIndex] = 'X';
-                    console.log(`\t\t\t X's turn!`);
+                    document.getElementById(`b-${moveI}`).innerHTML = 'X'
+                    moves[moveI] = 'X';
                     headerText.innerHTML = `Turn #${turnState+1}: O's turn`;
                 } else {
-                    document.getElementById(`b-${btnIndex}`).innerHTML = 'O'
-                    gameStat[btnIndex] = 'O';
-                    console.log(`\t\t\t O's turn!`);
+                    document.getElementById(`b-${moveI}`).innerHTML = 'O'
+                    moves[moveI] = 'O';
                     headerText.innerHTML = `Turn #${turnState+1}: X's turn`;
                 }
                 turnState++;
 
-                let winner = checkForWinner();
-                console.log(winner);
-                if (winner != '') {
+                let lead = winCheck();
+                if (lead != '') {
                     let headerText = document.getElementById('turn');
-                    headerText.innerHTML = `Game over!  ${winner}`;
-                    console.log(`Game over! ${winner}`);
+                    headerText.innerHTML = `Game over!  ${lead}`;
                 }
                 
             } else {
-                console.log('INVALID CHOICE');
+                console.log('error');
             }
         } else {
             turnState++;
@@ -64,44 +57,44 @@ for (let btnIndex=1; btnIndex <=9; btnIndex++) {
     });
 }
 
-function checkForWinner() {
-    let winner = '';
-    console.log(gameStat);
-    if ( (gameStat[1] === gameStat[2]) && (gameStat[1] === gameStat[3])
-         && (gameStat[1] !== '')) {
-        winner = `Winner is ${gameStat[1]}`;
-        gameStop = true;
-    } else if ( (gameStat[4] === gameStat[5]) && (gameStat[4] === gameStat[6])
-                && (gameStat[4] !== '')) {
-        winner = `Winner is ${gameStat[4]}`;
-        gameStop = true;
-    } else if ( (gameStat[7] === gameStat[8]) && (gameStat[7] === gameStat[9])
-                && (gameStat[7] !== '') ) {
-        winner = `Winner is ${gameStat[7]}`;
-        gameStop = true;
-    } else if ( (gameStat[1] === gameStat[4]) && (gameStat[1] === gameStat[7]) 
-                && (gameStat[1] !== '')) {
-        winner = `Winner is ${gameStat[1]}`;
-        gameStop = true;
-    } else if ( (gameStat[2] === gameStat[5]) && (gameStat[2] === gameStat[8])
-                && (gameStat[2] !== '')) {
-        winner = `Winner is ${gameStat[2]}`;
-        gameStop = true;
-    } else if ( (gameStat[3] === gameStat[6]) && (gameStat[3] === gameStat[9])
-                && (gameStat[3] !== '')) {
-        winner = `Winner is ${gameStat[3]}`;
-        gameStop = true;
-    } else if ( (gameStat[1] === gameStat[5]) && (gameStat[1] === gameStat[9])
-                && (gameStat[1] !== '')) {
-        winner = `Winner is ${gameStat[1]}`;
+function winCheck() {
+    let lead = '';
+    console.log(moves);
+    if ( (moves[1] === moves[2]) && (moves[1] === moves[3])
+         && (moves[1] !== '')) {
+        lead = `Winner is ${moves[1]}`;
+        gameEnd = true;
+    } else if ( (moves[4] === moves[5]) && (moves[4] === moves[6])
+                && (moves[4] !== '')) {
+        lead = `Winner is ${moves[4]}`;
+        gameEnd = true;
+    } else if ( (moves[7] === moves[8]) && (moves[7] === moves[9])
+                && (moves[7] !== '') ) {
+        lead = `Winner is ${moves[7]}`;
+        gameEnd = true;
+    } else if ( (moves[1] === moves[4]) && (moves[1] === moves[7]) 
+                && (moves[1] !== '')) {
+        lead = `Winner is ${moves[1]}`;
+        gameEnd = true;
+    } else if ( (moves[2] === moves[5]) && (moves[2] === moves[8])
+                && (moves[2] !== '')) {
+        lead = `Winner is ${moves[2]}`;
+        gameEnd = true;
+    } else if ( (moves[3] === moves[6]) && (moves[3] === moves[9])
+                && (moves[3] !== '')) {
+        lead = `Winner is ${moves[3]}`;
+        gameEnd = true;
+    } else if ( (moves[1] === moves[5]) && (moves[1] === moves[9])
+                && (moves[1] !== '')) {
+        lead = `Winner is ${moves[1]}`;
         gameOver = true;
-    } else if (( gameStat[3] === gameStat[5]) && (gameStat[3] === gameStat[7])
-                && (gameStat[3] !== '')) {
-        winner = `Winner is ${gameStat[3]}`;
-        gameStop = true;
+    } else if (( moves[3] === moves[5]) && (moves[3] === moves[7])
+                && (moves[3] !== '')) {
+        lead = `Winner is ${moves[3]}`;
+        gameEnd = true;
     } else if (turnState > 9){
-        winner = `The game is a draw!`;
-        gameStop = true;
+        lead = `The game is a draw!`;
+        gameEnd = true;
     }  
-    return winner;
+    return lead;
 }
