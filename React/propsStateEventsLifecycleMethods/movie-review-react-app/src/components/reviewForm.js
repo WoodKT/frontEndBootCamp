f37398842f0cd5e91348d9a65aef1ebf06df043d
WoodKT <soulsearcher14@gmail.com>
2022-02-07 23:02:41 -0500

    Update reviewForm.js
    adjust review form to be for review instead of log in

--- React/propsStateEventsLifecycleMethods/movie-review-react-app/src/components/reviewForm.js
+++ React/propsStateEventsLifecycleMethods/movie-review-react-app/src/components/reviewForm.js
@@ -8,12 +8,12 @@ export default class Form extends React.Component {
             <div className="container">
             <div className="card w-25 position-absolute top-50 start-50 translate-middle">
                 <div className="card-header bg-dark text-white border border-5 border-dark">
-                    <h3>Log In</h3>
+                    <h3>Review:</h3>
                 </div>
                 <div className="card-body border border-3 border-dark">
                     <User />
                 <div>
-                    <button type="submit" className="btn btn-success">Log In</button>
+                    <button type="submit" className="btn btn-success">Submit</button>
                 </div>
 
                 </div>
