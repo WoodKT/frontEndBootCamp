b3efde3336013d286a2d427ed6d107b9548bde80
WoodKT <soulsearcher14@gmail.com>
2022-06-27 21:54:31 -0400

    back to Basic CRUD functions
    removed old files, created basic CRUD functionality

--- React/movie-review-b/src/App.js
+++ React/movie-review-b/src/App.js
@@ -1,15 +1,30 @@
 import './App.css';
 import React from 'react';
-import MovieList from './components/movieList'
-import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import Create from './components/create';
+import Read from './components/read';
+import Update from './components/update';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
-    <div id='App'>
-      <div className='app'>
-        <MovieList />
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <div className="main">
+          <h2 className='main-header'>React CRUD Operations</h2>
+          <div>
+            <Route path='/create' element={Create} />
+          </div>
+          <div style={{ marginTop: 20 }}>
+              <Route path='/read' element={Read} />
+          </div>
+          <div>
+            <Route path='/update' element={Update} />
+          </div>
+        </div>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
