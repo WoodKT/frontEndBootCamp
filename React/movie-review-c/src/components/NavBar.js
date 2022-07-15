368855c281dae2c0bffe0d2fbf0b74f711b95c9f
WoodKT <soulsearcher14@gmail.com>
2022-07-15 00:50:30 -0400

    Update NavBar.js
    remove commented out code

--- React/movie-review-c/src/components/NavBar.js
+++ React/movie-review-c/src/components/NavBar.js
@@ -32,58 +32,7 @@ export default class MenuExampleSecondary extends Component {
           active={activeItem === 'update'}
           onClick={this.handleItemClick}
         />
-        {/* </Menu.Menu> */}
       </Menu>
     )
   }
 }
-// import React, { useState } from "react";
-// import { NavLink } from "react-router-dom";
-
-// export default function Navbar() {
-//     const [isOpen, setOpen] = useState(false);
-
-//   return (
-//     <nav
-//       className="navbar is-primary"
-//       role="navigation"
-//       aria-label="main navigation"
-//     >
-//       <div className="container">
-//         <div className="navbar-brand">
-//           <a
-//             role="button"
-//             className={`navbar-burger burger ${isOpen && "is-active"}`}
-//             aria-label="menu"
-//             aria-expanded="false"
-//             onClick={() => setOpen(!isOpen)}
-//           >
-//             <span aria-hidden="true"></span>
-//             <span aria-hidden="true"></span>
-//             <span aria-hidden="true"></span>
-//           </a>
-//         </div>
-
-//         <div className={`navbar-menu ${isOpen && "is-active"}`}>
-//           <div className="navbar-start">
-//             <NavLink
-//               className="navbar-item"
-//               activeClassName="is-active"
-//               to="/create"
-//             >
-//               Create
-//             </NavLink>
-
-//             <NavLink
-//               className="navbar-item"
-//               activeClassName="is-active"
-//               to="/read"
-//             >
-//               Read
-//             </NavLink>
-//           </div>
-//         </div>
-//       </div>
-//     </nav>
-//   );
-// };
