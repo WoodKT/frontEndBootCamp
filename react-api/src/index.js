e163c7a89100a489797c084a4086867001e2fa36
WoodKT <soulsearcher14@gmail.com>
2022-07-16 03:37:52 -0400

    new movie app
    using OMBD API, search movie, favorite and unfavorite movies

--- react-api/src/index.js
+++ react-api/src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
-reportWebVitals();
