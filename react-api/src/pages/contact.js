f712639bfe3228e004234a5e9761afbb68b744b9
WoodKT <soulsearcher14@gmail.com>
2022-07-18 01:19:07 -0400

    Update contact.js
    add contact form

--- react-api/src/pages/contact.js
+++ react-api/src/pages/contact.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import Form from 'react-bootstrap/Form';
 
-const Contact = () => {
-return (
-	<div>
-	<h1>Mail us on feedback@geeksforgeeks.org</h1>
-	</div>
-);
-};
+function Contact() {
+  return (
+    <Form>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="name@example.com" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Example textarea</Form.Label>
+        <Form.Control as="textarea" rows={3} />
+      </Form.Group>
+    </Form>
+  );
+}
 
 export default Contact;
