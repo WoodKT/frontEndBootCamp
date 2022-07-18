48e3414ed3a52d3ae6c03345b0fccf5074d956f6
WoodKT <soulsearcher14@gmail.com>
2022-07-17 23:12:06 -0400

    Update index.js
    implement bootstrap navbar

--- react-api/src/components/Navbar/index.js
+++ react-api/src/components/Navbar/index.js
@@ -1,28 +1,61 @@
-import React from "react";
-import { Nav, NavLink, NavMenu }
-	from "./NavbarElements";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
 
-const Navbar = () => {
-return (
-	<>
-	<Nav>
-		<NavMenu>
-		<NavLink to="/about" >
-			About
-		</NavLink>
-		<NavLink to="/contact" >
-			Contact Us
-		</NavLink>
-		<NavLink to="/movie" >
-			Movies
-		</NavLink>
-		<NavLink to="/sign-up" >
-			Sign Up
-		</NavLink>
-		</NavMenu>
-	</Nav>
-	</>
-);
-};
+function BasicExample() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand as={Link} to="/">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/movie">Movie</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item as={Link} to="/contact">Contact</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/blogs">
+                Blogs
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/about">About</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item as={Link} to="/signup">
+                Sign Up
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
 
-export default Navbar;
+export default BasicExample;
+
+// import React from 'react';
+// import { Link } from "react-router-dom";
+
+// const Navbar= () =>{
+//   return (
+//   <div>
+//     <li>
+//       <Link to="/">Home</Link>
+//     </li>
+//     <li>
+//       <Link to="/about">About</Link>
+//     </li>
+//     <li>
+//       <Link to="/movie">Movie</Link>
+//     </li>
+//     <li>
+//       <Link to="/contact">Contact</Link>
+//     </li>
+// 	<li>
+//       <Link to="/signup">Sign Up</Link>
+//     </li>
+//   </div>
+//   );
+// }
+// export default Navbar;
