586fbea74dee768cc132ed25476c1b275d857514
WoodKT <soulsearcher14@gmail.com>
2022-07-17 22:16:27 -0400

    Update index.js
    remove activeStyle from Navbar

--- react-api/src/components/Navbar/index.js
+++ react-api/src/components/Navbar/index.js
@@ -7,16 +7,16 @@ return (
 	<>
 	<Nav>
 		<NavMenu>
-		<NavLink to="/about" activeStyle>
+		<NavLink to="/about" >
 			About
 		</NavLink>
-		<NavLink to="/contact" activeStyle>
+		<NavLink to="/contact" >
 			Contact Us
 		</NavLink>
-		<NavLink to="/blogs" activeStyle>
+		<NavLink to="/blogs" >
 			Blogs
 		</NavLink>
-		<NavLink to="/sign-up" activeStyle>
+		<NavLink to="/sign-up" >
 			Sign Up
 		</NavLink>
 		</NavMenu>
