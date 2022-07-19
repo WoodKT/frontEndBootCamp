03b03672a4bc452436d8d7a3e7bf86973ba68d69
WoodKT <soulsearcher14@gmail.com>
2022-07-19 11:22:27 -0400

    Update index.js
    add post to api

--- react-api/src/pages/index.js
+++ react-api/src/pages/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const Home = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [checkbox, setCheckbox] = React.useState(false);
+  const postData = () => {
+      axios.post(`https://react-api.free.beeceptor.com/my/api/index`, {
+          email,
+          password,
+          checkbox
+      })
+  }
   return (
 	<div className='main'>
     <Form>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control type="email" placeholder="Enter email" onChange={(e) => setEmail(e.target.value)}/>
         <Form.Text className="text-muted">
           We'll never share your email with anyone else.
         </Form.Text>
@@ -16,12 +27,12 @@ const Home = () => {
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Agree Here" />
+        <Form.Check type="checkbox" label="Agree Here" onChange={(e) => setCheckbox(e.target.value)} />
       </Form.Group>
-      <Button className="btn btn-dark btn-block" type="submit">
+      <Button className="btn btn-dark btn-block" onClick={postData} type="submit">
         Log In
       </Button>
     </Form>
