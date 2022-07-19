e75df518e7279893c98c511838215d39b4e8de7b
WoodKT <soulsearcher14@gmail.com>
2022-07-19 11:22:21 -0400

    Update contact.js
    add post to api

--- react-api/src/pages/contact.js
+++ react-api/src/pages/contact.js
@@ -1,22 +1,33 @@
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function Contact() {
+  const [email, setEmail] = useState('');
+  const [concern, setConcern] = useState('');
+  const postData = () => {
+      axios.post(`https://react-api.free.beeceptor.com/my/api/contact`, {
+          // id : "{{faker 'random.uuid'}}",
+          email,
+          concern
+      })
+  }
   return (
     <div className='main'>
     <Form>
       <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
         <Form.Label>Email Address</Form.Label>
-        <Form.Control type="email" placeholder="name@example.com" />
+        <Form.Control type="email" placeholder="name@example.com" onChange={(e) => setEmail(e.target.value)} />
       </Form.Group>
       <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
         <Form.Label>Questions and Concerns</Form.Label>
-        <Form.Control as="textarea" rows={3} />
+        <Form.Control as="textarea" rows={3} onChange={(e) => setConcern(e.target.value)} />
       </Form.Group>
-      <Button type="submit">Submit</Button>
+      <Button onClick={postData} type="submit">Submit</Button>
     </Form>
     </div>
   );
-}
+};
 
 export default Contact;
