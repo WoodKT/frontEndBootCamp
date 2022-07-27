09979367907d651a0199a62a1c0e80398118b5fc
WoodKT <soulsearcher14@gmail.com>
2022-07-26 23:57:38 -0400

    Update contact.js
    change contact.js to be table and "read" data

--- react-api/src/pages/contact.js
+++ react-api/src/pages/contact.js
@@ -1,25 +1,155 @@
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Table, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+//import { contactUpdate } from '../components/contactUpdate';
 
-function Contact() {
-  return (
-    <div className='main'>
-      <div>
-    <Form>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="name@example.com" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-        <Form.Label>Example textarea</Form.Label>
-        <Form.Control as="textarea" rows={3} />
-      </Form.Group>
-      <Button type="submit">Submit</Button>
-    </Form>
-    </div>
-    </div>
+export default function Contact() {
+    const [APIData, setAPIData] = useState([]);
+    useEffect(() => {
+        axios.get(`https://react-api.free.beeceptor.com`)
+        .then((response) => {
+            setAPIData(response.data);
+        })
+    }, []);
 
-  );
+    const setData = (data) => {
+        let { id, email, feedback } = data;
+        localStorage.setItem('ID', id);
+        localStorage.setItem('Email', email);
+        localStorage.setItem('Feedback', feedback);
+    }
+
+    const getData = () => {
+        axios.get(`https://react-api.free.beeceptor.com`)
+            .then((getData) => {
+                setAPIData(getData.data);
+            })
+    }
+
+    const onDelete = (id) => {
+        axios.delete(`https://react-api.free.beeceptor.com/${id}`)
+        .then(() => {
+            getData();
+        })
+    }
+
+    return (
+        <div>
+            <Table striped bordered hover variant="dark">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Email</th>
+                        <th>Feedback</th>
+                        <th>Update</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {APIData.map((data) => {
+                        return (
+                            <tr key={data.id}>
+                                <td>{data.id}</td>
+                                <td>{data.email}</td>
+                                <td>{data.feedback}</td>
+                                    <td> 
+                                        <Button onClick={() => setData(data)}><Link to='/contactUpdate'>Update</Link></Button>
+                                    </td>
+                                <td>
+                                    <Button onClick={() => onDelete(data.id)}>Delete</Button>
+                                </td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </Table>
+        </div>
+    )
 }
+// import { useState, useEffect } from "react";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import ContactForm from "../components/contactForm";
+// import ContactList from "../components/ContactList";
 
-export default Contact;
+// export default function Contact() {
+//     here we create an array state to store the contact form data
+//     const [contacts, updateContacts] = useState([]);
+
+//     const addContact = (contact) => {
+//       updateContacts([...contacts, contact]);
+//     };
+//     const [contacts, setContacts] = useState([]);
+
+//     useEffect(() => {
+//       const movieContacts = JSON.parse(
+//         localStorage.getItem('react-movie-app-contacts')
+//       );
+
+//       if (movieContacts) {
+//         setContacts(movieContacts);
+//       }
+//     }, []);
+
+//     const saveToLocalStorage = (items) => {
+//       localStorage.setItem('react-movie-app-contacts', JSON.stringify(items));
+//     };
+
+//     const addContact = (movie) => {
+//       const newContactList = [...contacts, movie];
+//       setContacts(newContactList);
+//       saveToLocalStorage(newContactList);
+//     };
+
+//     const removeContact = (movie) => {
+//       const newContactList = contacts
+
+//       setContacts(newContactList);
+//       saveToLocalStorage(newContactList);
+//     };
+
+
+//     return (
+//         <div className="">
+//           <div>
+//             <ContactForm addContact={addContact} />
+//         </div><br/>
+//         <div>
+//             <ContactList
+//             contacts={contacts}
+// 					  handleContactsClick={removeContact}
+// 					  contactComponent={removeContact}
+//              />
+//         </div>
+//         </div>
+//     );
+//   }
+
+
+
+// import Form from 'react-bootstrap/Form';
+// import Button from 'react-bootstrap/Button';
+
+// function Contact() {
+//   return (
+//     <div className='main'>
+//       <div>
+//     <Form>
+//       <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+//         <Form.Label>Email address</Form.Label>
+//         <Form.Control type="email" placeholder="name@example.com" />
+//       </Form.Group>
+//       <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+//         <Form.Label>Example textarea</Form.Label>
+//         <Form.Control as="textarea" rows={3} />
+//       </Form.Group>
+//       <Button type="submit">Submit</Button>
+//     </Form>
+//     </div>
+//     </div>
+
+//   );
+// }
+
+// export default Contact;
