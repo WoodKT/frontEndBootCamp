d27a0e7fb00ae408a17a5b73f3beffabae8f8c45
WoodKT <soulsearcher14@gmail.com>
2022-07-26 23:56:30 -0400

    Create Navigation.js
    change name of previous index.js navigation bar file

---
+++ react-api/src/components/Navbar/Navigation.js
@@ -0,0 +1,31 @@
+import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+export default function Navigation() {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand as={Link} to="/">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/home">Home</Nav.Link>
+            <Nav.Link as={Link} to="/movie">Movie</Nav.Link>
+            <Nav.Link as={Link} to="/favorites">Favorites</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item as={Link} to="/contactForm">Contact</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/blogs">
+                Blogs
+              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/about">About</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item as={Link} to="/signup">
+                Sign Up
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
