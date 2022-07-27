6c204686132a879a6d00ca7fd43b4357b9a7a687
WoodKT <soulsearcher14@gmail.com>
2022-07-26 23:55:30 -0400

    Update ContactList.js
    change to link buttons

--- react-api/src/components/ContactList.js
+++ react-api/src/components/ContactList.js
@@ -1,69 +1,12 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { Table, Button } from 'semantic-ui-react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import Button from 'react-bootstrap/Button';
 
 export default function ContactList() {
-    const [APIData, setAPIData] = useState([]);
-    useEffect(() => {
-        axios.get(`https://react-api.free.beeceptor.com/my/api/contact/`)
-        .then((response) => {
-            setAPIData(response.data);
-        })
-    }, []);
-
-    const setData = (data) => {
-        let { id, email, concern } = data;
-        localStorage.setItem('ID', id);
-        localStorage.setItem('Email', email);
-        localStorage.setItem('Concern', concern);
-    }
-
-    const getData = () => {
-        axios.get(`https://react-api.free.beeceptor.com/my/api/contact/`)
-            .then((getData) => {
-                setAPIData(getData.data);
-            })
-    }
-
-    const onDelete = (id) => {
-        axios.delete(`https://react-api.free.beeceptor.com/my/api/contact/id}`)
-        .then(() => {
-            getData();
-        })
-    }
-
-    return (
-        <div>
-            <Table singleLine>
-                <Table.Header>
-                    <Table.Row>
-                        <Table.HeaderCell>ID</Table.HeaderCell>
-                        <Table.HeaderCell>Email</Table.HeaderCell>
-                        <Table.HeaderCell>Concern</Table.HeaderCell>
-                        <Table.HeaderCell>Update</Table.HeaderCell>
-                        <Table.HeaderCell>Delete</Table.HeaderCell>
-                    </Table.Row>
-                </Table.Header>
-
-                <Table.Body>
-                    {APIData.map((data) => {
-                        return (
-                            <Table.Row key={data.id}>
-                                <Table.Cell>{data.id}</Table.Cell>
-                                <Table.Cell>{data.email}</Table.Cell>
-                                <Table.Cell>{data.concern}</Table.Cell>
-                                    <Table.Cell> 
-                                        <Button onClick={() => setData(data)}><Link to='/update'>Update</Link></Button>
-                                    </Table.Cell>
-                                <Table.Cell>
-                                    <Button onClick={() => onDelete(data.id)}>Delete</Button>
-                                </Table.Cell>
-                            </Table.Row>
-                        )
-                    })}
-                </Table.Body>
-            </Table>
-        </div>
-    )
+  return (
+    <>
+      <Button href="/contactForm" type='button'>Form</Button>{' '}
+      <Button href="/contactUpdate" type='button'>Update</Button>{' '}
+      <Button href="/contact" type='button'>Contact Table</Button>
+    </>  
+  );
 }
